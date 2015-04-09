0bccdd0b75505570b905a3b431aa1e20dadef9de
gmatteo <gmatteo@gmail.com>
2015-04-09 23:48:58 +0200

    add aliases/custom.aliases.bash

---
+++ aliases/custom.aliases.bash
@@ -0,0 +1,19 @@
+# cd aliases
+alias cd..="cd .." 
+alias cd2="cd ../../" 
+alias cd3="cd ../../../" 
+
+# ls aliases
+alias ls1="ls .." 
+alias ls2="ls ../../" 
+alias ls3="ls ../../../"  
+alias ll='ls -l'
+alias lS='ls -lhS'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias cls="clear" 
+
+
+alias tf="tail -f" 
+alias mvim="/usr/mvim"
